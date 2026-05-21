c284df8eaf50870bdfc163e5f1f9badc434c7ff0
Shri-Leo <ashokarunsanthi@gmail.com>
2026-05-21 15:09:45 +0530

    Monte Carlo Simulation For A Single Match Completed

--- Src/Simulations/simulate_match.py
+++ Src/Simulations/simulate_match.py
@@ -16,10 +16,10 @@ LEAGUE_HOME_GOALS = 1.531
 LEAGUE_AWAY_GOALS = 1.164
 
 # =========================
-# SCORE MATRIX FUNCTION
+# SIMULATE MATCH
 # =========================
 
-def generate_score_matrix(home_team, away_team):
+def simulate_match(home_team, away_team):
 
     # Expected goals
     home_xg = (
@@ -27,12 +27,16 @@ def generate_score_matrix(home_team, away_team):
         * strengths.loc[away_team, "away_defense_strength"]
         * LEAGUE_HOME_GOALS
     )
-
+    
     away_xg = (
         strengths.loc[away_team, "away_attack_strength"]
         * strengths.loc[home_team, "home_defense_strength"]
         * LEAGUE_AWAY_GOALS
     )
+
+    # Random goals using Poisson
+    home_goals = np.random.poisson(home_xg)
+    away_goals = np.random.poisson(away_xg)
 
     print(f"\n{home_team} vs {away_team}")
     print("-" * 40)
@@ -40,40 +44,12 @@ def generate_score_matrix(home_team, away_team):
     print(f"Expected {home_team} Goals: {home_xg:.2f}")
     print(f"Expected {away_team} Goals: {away_xg:.2f}")
 
-    # =========================
-    # SCORE MATRIX
-    # =========================
+    print("\n===== SIMULATED RESULT =====")
 
-    max_goals = 5
-
-    matrix = []
-
-    for home_goals in range(max_goals + 1):
-        for away_goals in range(max_goals + 1):
-            probability = (
-                poisson.pmf(home_goals, home_xg) * poisson.pmf(away_goals, away_xg)
-            )
-
-            matrix.append({
-                "home_goals": home_goals,
-                "away_goals": away_goals,
-                "probability": probability
-            })
-
-    # Convert to dataframe
-    matrix_df = pd.DataFrame(matrix)
-
-    # Sort by highest probability
-    matrix_df = matrix_df.sort_values(
-        by="probability",
-        ascending=False
-    )
-
-    print("\n===== TOP 10 MOST LIKELY SCORES =====")
-    print(matrix_df.head(10))
+    print(f"{home_team} {home_goals} - {away_goals} {away_team}")
 
 # =========================
 # TEST
 # =========================
 
-generate_score_matrix("Arsenal", "Chelsea")
+simulate_match("Liverpool", "Man City")
